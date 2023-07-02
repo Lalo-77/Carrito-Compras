fe7e6dbdf17d3d1e58938f9434eed1a7f8176440
cristian nunez <crisn3682@gmail.com>
2023-07-02 10:57:52 -0300

    hice cambios en la pagina del proyecto y cree el registro.js y registro.html

---
+++ js/registro.js
@@ -0,0 +1,8 @@
+const btnCrearCta=document.querySelector("#btn-crearCta");
+      btnCrearCta.addEventListener("click", () =>{
+      console.log("Se ha creado tu cuenta")
+    });
+const btnLimpiar=document.querySelector(".btnLimpiar");
+      btnLimpiar.addEventListener("click", () =>{
+      console.log("Se eliminaron los datos")
+     });
