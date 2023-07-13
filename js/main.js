ff440affc63b4cb195cb928a589827be5e6c118c
cristian nunez <crisn3682@gmail.com>
2023-07-13 12:54:10 -0300

    hcie cambios en el js del rpoyecto Carrito-Compras

--- js/main.js
+++ js/main.js
@@ -1,82 +1,3 @@
-const plato = [
-{
-  id: 1,
-  nombre: "La fortaleza",
-  img: "./assets/img/brooke.jpg",
-  precio: 2000,
-  description: "calorias:150,carbohidratos:60,fibras:30,grasasSaturadas:0,",
-  categoria: "break",
-},
-{id: 2,nombre: "Especial",
-img: "./assets/img/Especial.jpg",
-precio: 2300,description: " calorias:150,carbohidratos:30,fibras:40,grasasSaturadas:0,",
-categoria: "entrada",
-},
-{
-  id: 3,
-  nombre: "Rubi",
-  img: "./assets/img/Rubi.jpg",
-  precio: 2500,
-  description: "calorias:110,carbohidratos:40,fibras:20,grasasSaturadas:0,",
-  categoria: "plato principal",
-},
-{
-  id: 4,
-  nombre: "Hamburguesa",
-  img: "./assets/img/Hamburguesa.jpg",
-  precio: 2500,
-  description: "calorias:130,carbohidratos:30,fibras:20,grasasSaturadas:0,",
-  categoria: "entrada",
-},
-{
-  id: 5,
-  nombre: "Jugo de zanahoria",
-  img: "./assets/img/jugo de zanahoria.jpg",
-  precio: 1200,
-  description: "calorias:130,carbohidratos:70,fibras:40,grasasSaturadas:0,",
-  categoria: "jugo",
-},
-{
-  id: 6,
-  nombre: "Ensalada de palta queso tomate y cebolla",
-  img: "./assets/img/varios.jpg",
-  precio: 1800,
-  description: "calorias:120,carbohidratos:30,fibras:20,grasasSaturadas:0,",
-  categoria: "Plato principal",
-},
-{
-  id: 7,
-  nombre: "Pastel de brocoli con queso",
-  img: "./assets/img/especialidad2.jpg",
-  precio: 1700,
-  description: "calorias:100,carbohidratos:40,fibras:10,grasasSaturadas:0,",
-  categoria: "entrada",
-},
-{
-  id: 8,
-  nombre: "Pollo salteado con brocoli y soja",
-  img: "./assets/img/especialidad3.jpg",
-  precio: 1600,
-  description: " calorias:80,carbohidratos:40,fibras:20,grasasSaturadas:0,",
-  categoria: "plato principal",
-},
-{
-  id: 9,
-  nombre: "Salmon con arroz cebolla y pepino",
-  img: "./assets/img/especialidad5.jpg",
-  precio: 1700,
-  description: "calorias:150,carbohidratos:50,fibras:15,grasasSaturadas:0,",
-  categoria: "plato principal",
-},
-{
-  id: 10,
-  nombre: "Frutas",
-  img: "./assets/img/especialidad6.jpg",
-  precio: 1800,
-  description: "calorias:50,carbohidratos:20,fibras:10,grasasSaturadas:0,",
-  categoria: "postre",
-},
-];
 const carrito = [];
 const contenedorPlatos = document.querySelector("#contenedorPlatos");
 const contenedorCarrito=document.querySelector("#contenedorCarrito");
@@ -267,7 +188,7 @@ const btnLimpiar=document.querySelector(".btnLimpiar");
       btnLimpiar.addEventListener("click", () =>{
       console.log("Se elimino el plato")
 });
-const btnEliminar=document.querySelector(".btnEliminar");
+const btnEliminar=document.querySelector(".btn-borrar");
       btnEliminar.addEventListener("click", () =>{
       console.log("Se elimino el plato")
 });
